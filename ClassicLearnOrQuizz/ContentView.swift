b9018295e77ba8a1b2f7457458d47ab7ccb9b431
pat <patrick.philipt@gmail.com>
2020-07-14 07:21:21 +0200

    Initial Commit

---
+++ ClassicLearnOrQuizz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClassicLearnOrQuizz
+//
+//  Created by patrick philipot on 14/07/2020.
+//  Copyright © 2020 stgpcs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
